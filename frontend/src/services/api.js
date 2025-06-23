bc568b84f78be4c7656f2a0a8dc6198bb8987d0b
ravisain <118372906+Ravisain7851051298@users.noreply.github.com>
2025-06-23 16:16:59 +0530

    ready for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/chat';
+const API_URL = 'https://chatgpt-mini-zo1o.onrender.com/api/chat';
 
 export async function sendMessage(message) {
   try {
